b7230df70375212073f8265e36d1cf011e241a00
Nicholas MacRae <nmacrae0@gmail.com>
2020-10-24 14:35:25 -0400

    Intial commit, starting by creating the basic functions newProject and update.  Both are skeleton functions.

---
+++ model/backendDefs.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class Project:
+# This will contain the functions associated wtih the different projects.  It should contain methods to create new,
+#     update current
+    def newProject(projectname):
+        #decide if create a new project with just the name or have the items that are initially required.
+        return None
+
+    def updateProject(self):
+
+        return None

bfb788363bd44f755c442bb071c60461fefe9218
Nicholas MacRae <nmacrae0@gmail.com>
2020-10-25 00:59:36 -0400

    Updated the init for both classes to have the required items.

--- model/backendDefs.py
+++ model/backendDefs.py
@@ -3,18 +3,28 @@ from datetime import date
 #keys for the dictionary in JS should be projectName, devList, tags, date, active, description, manager
 #may need functionality for the removal of devs
 
+
+#active(boolean), created_date (date), description (text), dev_list(list<text>)., manager (text),
+# name (text), tags(set<text>)
+
 class Project:
 
-#projectList is the total list of prejects (the total object), tags is the list of currentTags,
-#current Developer list is the list of the current delevopers for the project, description is the descritption of the
-#current project, active is if the project is currently active,
+#active == active and is a boolean
+#created  is the date that it was created == created_date is a date (assuming text)
+#description is the project description == description
+#developerlist is the list of current developers on the project == dec_list [MUST BE CONVERTED TO SET]
+#manager is the manager of the current project == manager
+#project is the name of the current project == name
+#tags is the set of current tags == tags
 
-    def __init__(self, projectList,currentTags, developerList, description, active):
+    def __init__(self, projectList ,active, created, description, developerList, manager, project, tags):
         self.projectList = projectList
-        self.currentTags = currentTags
-        self.devloperList = developerList
-        self.description = description
         self.active = active
+        self.createdDate = created
+        self.description = description
+        self.devlopers = developerList
+        self.manager = manager
+        self.projectName = project
 
 #this is going to take in the currrent List of addDevList, the set will not allow duplicates so no  checking needed
     def updateAddDevList(self, addDevList):
@@ -42,28 +52,45 @@ class Project:
             currentTagSet.discard(tag)
         return currentTagSet
 
-    def updateActive(self):
-        return 0
+#This is going to take in a boolean value only
+    def updateActive(self, value):
+        currentStatus = self.active
+        currentStatus = value
+        return currentStatus
 
+#for now this function will completely the current description.
     def updateDescription(self):
-        return 0
+
+        return
 
     def UpdateManager(self):
         return 0
 
 
 
+#email (text) first_name(text), joint_ date (date), last_name (text) last_session(timestamp)
+# projects(set<text>), tags(set<text>)
 class Dev:
 
-#developer list is the list of developers, developer should be the email, tags is the set of tags, current projects is the set of current projects,
-#lastSession is the last time the develper was online
+#developer list is the list of developers, developer should be the email == email
+#tags is the set of tags == tags
+# current projects is the set of current projects == projects
+#lastSession is the last time the develper was online this is a timestamp
+#lastName == last_name
+#firstName == first_name
+#join_date == joinDate
 
-    def __init__(self, developerlist, developer, tags, currentProjects, lastSession):
+
+    def __init__(self, developerlist, firstName, joinDate, lasName, developer, tags, currentProjects, lastSession):
+        self.developerList = developerlist
+        self.firstname = firstName
+        self.joinDate = joinDate
+        self.lastName = lasName
         self.developer = developer
         self.tags = tags
         self.projects = currentProjects
         self.lastSession = lastSession
-        self.developerList = developerlist
+        self.developerList = developerlist  #this is the list of all developers does this need to be removed?
 
     def getDevList(self):
         devs = self.developerList

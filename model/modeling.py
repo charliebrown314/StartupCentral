c9475c7f4d2a3a5f99b539f217e9f5899574e56c
Jacob Snyderman <jacob.snyderman@gmail.com>
2020-10-25 05:53:36 -0400

    requirements.txt

--- model/modeling.py
+++ model/modeling.py
@@ -1,6 +1,3 @@
-import pandas as pd
-import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
---
+++ sample_data/samples.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+from model.backendDefs import Project
+
+p = Project(True, datetime.now(),"A Startup Centric Social Media Application to Encourage Creative Collisions", ["jwbrown4@buffalo.edu", "jacobsny@buffalo.edu", "jmacias@buffalo.edu", "nmacrae@buffalo.edu"],"jacobsny@buffalo.edu","StartupCentral",["python"])
